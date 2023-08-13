7b241a37ce7f76d890af29bb9b3541f99bfff4ca
Eric White <eric.white.austin@gmail.com>
2023-08-13 17:01:36 -0500

    Rework TopBar.

--- src/GitHub.tsx
+++ src/GitHub.tsx
@@ -1,25 +1,26 @@
-import {Stack, Typography} from "@mui/material";
+import {Hidden, Stack, Typography} from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import React from "react";
 
 export function GitHub() {
-    return <div style={{
-        position: "fixed",
-        top: 0,
-        right: 10,
-        zIndex: 1,
-        fontSize: 50,
-        color: "white",
-    }}>
-        <a href="https://github.com/SuperNerdEric/osrs-dps" target="_blank" style={{textDecoration: "none"}}
-           rel="noreferrer">
-            <Stack direction="row" alignItems="center" gap={1}>
-                <GitHubIcon style={{
-                    fontSize: 45,
-                    color: "white"
-                }}/>
-                <Typography style={{color: "white", fontSize: "35px"}}>GitHub</Typography>
-            </Stack>
-        </a>
-    </div>;
+    return (
+        <div style={{
+            fontSize: 50,
+            color: "white",
+            textAlign: 'right',
+        }}>
+            <a href="https://github.com/SuperNerdEric/osrs-dps" target="_blank" style={{textDecoration: "none"}}
+               rel="noreferrer">
+                <Stack direction="row" alignItems="center" gap={1}>
+                    <GitHubIcon style={{
+                        fontSize: 45,
+                        color: "white"
+                    }}/>
+                    <Hidden smDown>
+                        <Typography style={{color: "white", fontSize: "35px"}}>GitHub</Typography>
+                    </Hidden>
+                </Stack>
+            </a>
+        </div>
+    );
 }
--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -16,32 +16,41 @@ const TopBar: React.FC<TopBarProps> = ({urlState, setUrlState}) => {
     return (
         <header>
             <AppBar position="static" style={{background: '#000'}}>
-                <Toolbar>
+                <Toolbar style={{paddingLeft: 24, paddingRight: 10}}>
                     <Hidden smUp>
-                        <DrawerMenu sections={sections} setTargetMonster={(targetMonster: TargetMonster) => {
-                            setUrlState({
-                                target: targetMonster.shortName || targetMonster.name,
-                                invocationLevel: targetMonster.raid === Raid.TombsOfAmascut ? urlState.invocationLevel : undefined,
-                                defenceReduction: urlState.defenceReduction,
-                            });
-                        }}/>
+                        <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+                            <DrawerMenu sections={sections} setTargetMonster={(targetMonster: TargetMonster) => {
+                                setUrlState({
+                                    target: targetMonster.shortName || targetMonster.name,
+                                    invocationLevel: targetMonster.raid === Raid.TombsOfAmascut ? urlState.invocationLevel : undefined,
+                                    defenceReduction: urlState.defenceReduction,
+                                });
+                            }}/>
+                            <div>
+                                <GitHub/>
+                            </div>
+                        </div>
                     </Hidden>
                     <Hidden smDown>
-                        <Box display={{xs: 'none', sm: 'block'}}>
-                            <Grid container spacing={2}>
-                                {sections.map((section, index) => (
-                                    <Grid item key={index}>
-                                        <TopBarItem setTargetMonster={(targetMonster: TargetMonster) => {
-                                            setUrlState({
-                                                target: targetMonster.shortName || targetMonster.name,
-                                                invocationLevel: targetMonster.raid === Raid.TombsOfAmascut ? urlState.invocationLevel : undefined,
-                                                defenceReduction: urlState.defenceReduction,
-                                            });
-                                        }} monsterList={section.monsters} sectionName={section.name}/>
-                                    </Grid>
-                                ))}
-                                <GitHub/>
-                            </Grid>
+                        <Box display={{xs: 'none', sm: 'block'}} width="100%" height="100%">
+                            <div style={{display: 'flex'}}>
+                                <Grid container spacing={2} style={{flexGrow: 1}}>
+                                    {sections.map((section, index) => (
+                                        <Grid item key={index}>
+                                            <TopBarItem setTargetMonster={(targetMonster: TargetMonster) => {
+                                                setUrlState({
+                                                    target: targetMonster.shortName || targetMonster.name,
+                                                    invocationLevel: targetMonster.raid === Raid.TombsOfAmascut ? urlState.invocationLevel : undefined,
+                                                    defenceReduction: urlState.defenceReduction,
+                                                });
+                                            }} monsterList={section.monsters} sectionName={section.name}/>
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                                <div>
+                                    <GitHub/>
+                                </div>
+                            </div>
                         </Box>
                     </Hidden>
                 </Toolbar>
