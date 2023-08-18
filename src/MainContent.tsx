bbdf870988e07041e5bd542aad56d25aad87711b
Eric White <eric.white.austin@gmail.com>
2023-08-18 18:11:21 -0500

    Remove unused function.

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -47,11 +47,6 @@ const MainContent: React.FC<MainContentProps> = ({
         handleOnTask(player.onTask);
     }, [player.onTask]);
 
-
-    const handleMonsterSelect = (monster: TargetMonster) => {
-        setTargetMonster(monster);
-    };
-
     useEffect(() => {
         const results: Calculator[] = [];
 
