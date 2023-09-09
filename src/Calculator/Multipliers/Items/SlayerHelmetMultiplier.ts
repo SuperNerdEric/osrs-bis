ec11d157aa504d73dd52f2afb6518b30fc2001c4
Eric White <eric.white.austin@gmail.com>
2023-09-09 12:28:20 -0500

    Use combat class for SlayerHelmetMultiplier.ts

--- src/Calculator/Multipliers/Items/SlayerHelmetMultiplier.ts
+++ src/Calculator/Multipliers/Items/SlayerHelmetMultiplier.ts
@@ -1,11 +1,11 @@
 import {Calculator} from "../../Calculator";
 import {ItemName} from "../../DataObjects/ItemName";
-import {StyleType} from "../../DataObjects/Item";
+import {CombatClass} from "../../DataObjects/Item";
 
 export function slayerHelmetMultiplier(calculator: Calculator): number {
     const slayerHelmetPresent = calculator.gearSet.hasItemByName(ItemName.SlayerHelmetI);
     if (slayerHelmetPresent && calculator.player.onTask) {
-        if (calculator.gearSet.styleType == StyleType.Stab || calculator.gearSet.styleType == StyleType.Slash || calculator.gearSet.styleType == StyleType.Crush) {
+        if (calculator.gearSet.combatClass == CombatClass.Melee) {
             return 7 / 6;
         } else {
             return 1.15;
