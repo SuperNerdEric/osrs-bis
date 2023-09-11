624a181ac9062085a6e71313446966d58f5dafa4
Eric White <eric.white.austin@gmail.com>
2023-09-11 01:44:39 -0500

    Add comment.

--- src/Calculator/Modifiers/Additive/Items/AmuletOfAvariceAddend.ts
+++ src/Calculator/Modifiers/Additive/Items/AmuletOfAvariceAddend.ts
@@ -1,6 +1,10 @@
 import {Calculator} from "../../../Calculator";
 import {ItemName} from "../../../DataObjects/ItemName";
 
+/**
+ * Note: Avarice damage with Melee and Ranged is multiplicative, not additive.
+ * For its multiplicative behavior with Melee and Ranged, refer to {@link amuletOfAvariceMultiplier}
+ */
 export function amuletOfAvariceAddend(calculator: Calculator): number {
     if (calculator.gearSet.hasItemByName(ItemName.AmuletOfAvarice) && calculator.targetMonster.name.includes("Revenant")) {
         return 20;
