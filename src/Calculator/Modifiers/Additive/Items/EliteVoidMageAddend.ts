ff94eaa3aabcab5893b1e21dc3a7c9a995ccacaf
Eric White <eric.white.austin@gmail.com>
2023-09-11 01:46:40 -0500

    Switch to TSdoc comment.

--- src/Calculator/Modifiers/Additive/Items/EliteVoidMageAddend.ts
+++ src/Calculator/Modifiers/Additive/Items/EliteVoidMageAddend.ts
@@ -10,7 +10,9 @@ const EliteVoidMageSet = [
 
 const EliteVoidMageBonus = 2.5;
 
-//Happens after Tumeken's even though it shows up on the stat sheet https://archive.ph/mw2LB https://archive.ph/CtG0m
+/**
+ * Happens after Tumeken's even though it shows up on the stat sheet https://archive.ph/mw2LB https://archive.ph/CtG0m
+ */
 export function eliteVoidMageAddend(calculator: Calculator): number {
     if (EliteVoidMageSet.every(item => calculator.gearSet.hasItemByName(item))) {
         return EliteVoidMageBonus;
